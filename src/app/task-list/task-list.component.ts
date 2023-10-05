f0b042597affe38561bb57943ca1d8b5b6ca643d
Robert Andrew Moore <ra.moore573@gmail.com>
2023-10-04 19:44:50 -0500

    Think I'm ready for Thursday's class

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
+  taskList = [{
+    title: "Get Angular Project Started",
+    dueDate: '10/1/2023',
+    priority: 'High',
+    status: 'In Progress'
+  },{
+    title: "Clean Carport",
+    dueDate: '10/15/2023',
+    priority: 'Low',
+    status: 'Not Started'
+  },{
+    title: "Get Oil Changed",
+    dueDate: '10/25/2023',
+    priority: 'High',
+    status: 'Completed'
+  }];
+
+
 
 }
